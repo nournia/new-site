5feea898c88014758df4eb3135e6d0133be56e2a
mehrdadsaffarie <mehrdadsaffarie@gmail.com>
2016-12-24 14:19:03 +0330

    Dey Changes
    OpenSource and Idea sections added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-$(function(){
-    $("#typed").typed({
-        strings: ["نویسه خوان الف", "برای خواندن فارسی", "انگلیسی و عربی"],
-        typeSpeed: 30,
-        loop: true
+$(document).ready(function(){
+    $(function(){
+        $("#typed").typed({
+            strings: ["نویسه خوان الف", "برای خواندن فارسی", "انگلیسی و عربی"],
+            typeSpeed: 30,
+            loop: true
+        });
     });
+
+    $(".idea-block").hover(function(){
+        var block = $(this);
+        block.find("img").animate({"marginTop" : 10}, 500, 'easeOutCirc');
+        block.find("h3").animate({"marginTop" : 30}, 500, 'easeOutCirc');
+        block.find("p").toggleClass("visible");
+    },
+    function(){
+        var block = $(this);
+        block.find("img, h3").animate({"marginTop" : 70}, 500, 'easeOutCirc');
+        block.find("p").toggleClass("visible");
+    });
+
 });
